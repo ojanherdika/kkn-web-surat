15a7a5c744771ca943f50675e867d1a277b24a2b
Fauzan Herdika Tubagus Putra <47662675+ojanherdika@users.noreply.github.com>
2020-08-28 21:51:55 +0700

    fix some shit

--- application/views/admin/profilPage.php
+++ application/views/admin/profilPage.php
@@ -77,34 +77,23 @@
                     <option value="44">RT 14</option>
                 </select>
             </div>
-
             <div class="col-md-1">
                 <button type="button" class="btn btn-primary"> Tampil </button>
             </div>
-
         </div>
-
-
-
         <br>
-
-
         <div class="card mb-3">
             <div class="card-body">
                 <div class="table-responsive" style="overflow-x: hidden;">
                     <table class="table  table-striped table bordered mydatatable" id="dataTable" width="100%" cellspacing="0">
                         <thead>
                             <tr>
-                                <th scope="col"><?= $i++; ?></th>
-                                <th scope="col"><?= $user['no_kk']; ?></th>
-                                <th scope="col"><?= $user['no_nik']; ?></th>
-                                <th scope="col"><?= $user['nama']; ?></th>
-                                <th scope="col"><?= $user['RT']; ?></th>
-                                <th scope="col"><?= $user['RW']; ?></th>
-                                <th scope="col">
-                                    <a style="-moz-tab-size: 2;" class="btn btn-info m-1 " href="<?= base_url('admin/detail_warga/' . $user['id']); ?>"> Detail </a>
-                                    <a style="-moz-tab-size: 2;" class="btn btn-danger m-1 " onclick="" class="btn btn-small" data-toggle="modal" data-target="#staticModal" data-popup="tooltip" data-placement="top" title="Hapus Data"> Hapus </a>
-                                </th>
+                                <th scope="col">No</th>
+                                <th scope="col">No.KK</th>
+                                <th scope="col">No.NIK</th>
+                                <th scope="col">Nama Lengkap</th>
+                                <th scope="col">RT</th>
+                                <th scope="col">RW</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -119,8 +108,7 @@
                                     <th scope="col"><?= $user['RT']; ?></th>
                                     <th scope="col"><?= $user['RW']; ?></th>
                                     <th scope="col">
-                                        <a style="-moz-tab-size: 2;" class="btn btn-primary text-white" href="<?php echo base_url('admin/detail_warga' . $user['id']); ?>"> <u> Detail </u> </a>
-
+                                        <a style="-moz-tab-size: 2;" class="btn btn-primary text-white" href="<?php echo base_url('admin/detail_warga/' . $user['id']); ?>"> <u> Detail </u> </a>
                                         &emsp;
                                         <a style="-moz-tab-size: 2;" class="btn btn-danger m-1 text-white "> Hapus </a> <!-- fungsi hapus belum masuk controller -->
                                     </th>
@@ -131,12 +119,6 @@
                 </div>
             </div>
         </div>
-
-
-
-    </div>
-    <!-- /.container-fluid -->
-
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
